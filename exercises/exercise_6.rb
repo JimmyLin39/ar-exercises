e7a8eb124bc4f9cb7249bee26533d208b7a6a1fc
JimmyLin39 <jimmylin0309@email.com>
2017-12-05 14:22:00 -0800

    One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+ 
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Georgina", last_name: "Mamoun", hourly_rate: 20)
+@store2.employees.create(first_name: "Jacob", last_name: "Irakliy", hourly_rate: 50)
+@store2.employees.create(first_name: "Arnie", last_name: "Lynn", hourly_rate: 30)
+pp Employee.all
